8154d2d432adc292c0d29daa161f14d7c3f6bcd1
lingbopro <ilovemc@zohomail.cn>
2024-12-07 18:09:28 +0800

    chore: add husky & commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+export default {
+  // NOTE: we follow the Conventional Commits specification
+  // view full specification on https://www.conventionalcommits.org/en/v1.0.0
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // types declared in Conventional Commits specification & @commitlint/config-conventional
+    // see: https://www.conventionalcommits.org/en/v1.0.0/#summary
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'scope-enum': [
+      2,
+      'always',
+      // scopes
+      // see: https://www.conventionalcommits.org/en/v1.0.0/#specification
+      [
+        // folder name in src/
+        'core',
+        'utils',
+        // component names
+        // add component name here when adding new component
+        'page',
+      ],
+    ],
+  },
+};
