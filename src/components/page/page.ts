670fa4b3917214ebc36f26d5c15f0cae2a4d52ec
lingbopro <ilovemc@zohomail.cn>
2024-12-07 16:21:07 +0800

    feat(page): add page component
    Page - the root element
    our first component!

---
+++ src/components/page/page.ts
@@ -0,0 +1,40 @@
+import { useElement } from '../../core/element';
+import { defaultTheme, generateCSSKeys, overrideColorKeys } from '../../utils/theme';
+import template from './page.html';
+import style from './page.css';
+
+const name = 'mm-page';
+const props = {
+  theme: 'auto' as 'auto' | 'light' | 'dark',
+};
+
+const themeStyle = /* css */ `
+:host {
+  ${generateCSSKeys(defaultTheme)}
+}
+@media (prefers-color-scheme: light) {
+  :host {
+    ${overrideColorKeys.light}
+  }
+}
+@media (prefers-color-scheme: dark) {
+  :host {
+    ${overrideColorKeys.dark}
+  }
+}
+:host([theme="light"]) {
+  ${overrideColorKeys.light}
+}
+:host([theme="dark"]) {
+  ${overrideColorKeys.dark}
+}`;
+
+export class Page extends useElement({
+  template,
+  style: [style, themeStyle],
+  props,
+}) {
+  declare theme: typeof props.theme;
+}
+
+Page.define(name);
